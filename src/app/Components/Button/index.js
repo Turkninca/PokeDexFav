1696f90d978e4fb94d03b20ef8e3dbda0f196dd6
Turkninca <menque7046@gmail.com>
2025-06-19 17:22:30 +0300

    Update Styling

--- src/app/Components/Button/index.js
+++ src/app/Components/Button/index.js
@@ -1,23 +1,25 @@
 'use client';
 
-import '../../Styles/components/Button.css'
+import pokemonStore from '@/app/stores/mobxStore';
+import './Button.css'
 import { useRouter } from 'next/navigation';
 
 export default function RegisterButton({ type = "SignIn" }) {
     const router = useRouter();
 
     const handleClick = () => {
+        pokemonStore.reset([])
         router.push(type);
     }
 
     return(
         <button className='button-container' onClick={handleClick}>
-            <div className={`buttonbg ${type == 'SignIn' ? 'bg-[#5562eb] hover:bg-[#3a49b4]' : 'transparent'}`}>
-                <span className={`opacity-100 ${type == 'SignIn' ? 'text-[#ffffff]' : 'text-[#5562eb]'}`}>
+            <div className={`buttonbg ${type == 'SignIn' ? 'bg-[#5CBC94]' : 'bg-[#425385]'}`}>
+                <span className={`${type == 'SignIn' ? 'text-[#FDF4ED]' : 'text-[#FDF4ED]'}`}>
                     {type == 'SignIn' ? 'Sign In' : 'Login'}
                 </span>
             </div>
-            <div className={`shadow ${type == 'SignIn' ? 'bg-gray-500' : 'bg-gray-200'}`}/>
+            <div className={`shadow ${type == 'SignIn' ? 'bg-[#45A185]' : 'bg-[#28257E]'}`}/>
         </button>
     )
 }
