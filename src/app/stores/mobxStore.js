d65defc52637f81c4576da321f68493ed2be9a82
Turkninca <menque7046@gmail.com>
2025-06-19 17:35:42 +0300

    Update Rendering Algorithm & Add Details Store

--- src/app/stores/mobxStore.js
+++ src/app/stores/mobxStore.js
@@ -2,12 +2,82 @@ import axios from "axios";
 import pokemonInstance from "./instance";
 import { makeAutoObservable, reaction } from "mobx";
 
+class DomStore {
+    mainInputRef;
+    dropdown = false;
+    slider = false;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    setDropdown(bool) {
+        this.dropdown = bool
+    }
+
+    setSlider(bool) {
+        this.slider = bool
+    }
+
+    setRef(ref) {
+        this.mainInputRef = ref
+    }
+}
+
+class DetailsStore {
+    pokemonSpecies = new Object
+    pokemonDetails = new Object
+    fetching = true
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    toggleFetching(bool) {
+        this.fetching = bool
+    }
+
+    async fetchSpeciesDetails(pokemonName) {
+        const speciesResponse = await pokemonInstance.get(`/pokemon-species/${pokemonName}`)
+
+        this.setSpecies(speciesResponse.data)
+    }
+
+    async fetchPokemonDetails(pokemonName) {
+        const pokemonResponse = await pokemonInstance.get(`/pokemon/${pokemonName}`)
+
+        this.setPokemon(pokemonResponse.data)
+    }
+
+    async fetchPokemon(pokemonName) {
+        this.toggleFetching(true)
+        await this.fetchPokemonDetails(pokemonName)
+        await this.fetchSpeciesDetails(pokemonName)
+        this.toggleFetching(false)
+    }
+
+    reset() {
+        this.setPokemon(new Object)
+        this.setSpecies(new Object)
+        this.toggleFetching(true)
+    }
+
+    setPokemon(detailData)  {
+        this.pokemonDetails = detailData
+    }
+
+    setSpecies(speciesData) {
+        this.pokemonSpecies = speciesData
+    }
+}
+
 class PokemonStore {
     pokemonData = [];
     loading = true;
-    nextUrl = "";
+    nextUrl = `/pokemon/?limit=12&offset=${(this.pageNumber - 1) * 12}`;
     filterInput = ""
-    pageNumber = 1
+    pageNumber = 0
+    sorted = []
 
     constructor() {
         makeAutoObservable(this);
@@ -16,6 +86,10 @@ class PokemonStore {
     setPokemonData(data) {
         this.pokemonData = data;
         this.loading = false;
+    }
+
+    setSorted(sortedData) {
+        this.sorted = sortedData;
     }
 
     setFilterInput(text) {
@@ -30,19 +104,27 @@ class PokemonStore {
         this.nextUrl = url;
     }
 
+    reset() {
+        this.pokemonData = []
+        this.pageNumber = 0
+        this.filterInput = ""
+        this.nextUrl = `/pokemon/?limit=12&offset=${(this.pageNumber - 1) * 12}`
+    }
+
     async filtered() {
         console.log("Filtering for: ", this.filterInput);
         this.loading = true;
         const response = await pokemonInstance.get("/pokemon?limit=1025")
-        console.log(response.data.results);
+        // console.log(response.data.results);
         const filteredList = response.data.results.filter(poke => poke.name.includes(this.filterInput))
-        console.log("Page Number: ", this.pageNumber);
+        // console.log("Page Number: ", this.pageNumber);
         const detailed = await Promise.all(
             filteredList.slice(0, this.pageNumber * 12).map(poke => axios.get(poke.url).then(res => res.data))
         );
 
         this.setPokemonData(detailed)
-        console.log(this.pokemonData);
+        this.setSorted(pokemonStore.pokemonData.sort((a, b) => a.id - b.id))
+        // console.log(this.pokemonData);
     }
 
     async fetchPokemonData(requestUrl) {
@@ -59,7 +141,8 @@ class PokemonStore {
         )
 
         this.setNextUrl(response.data.next)
-        this.setPokemonData(this.pokemonData.concat(detailedList))
+        this.setPokemonData(detailedList)
+        this.setSorted(pokemonStore.pokemonData.sort((a, b) => a.id - b.id))
         //this.pokemonData.push(...detailedList)
         //this.setPokemonData(detailedList)
     }
@@ -71,8 +154,9 @@ class PokemonStore {
         if (isSearchButton) {
             if (this.filterInput == "") {
                 console.log("True");
+                console.log(this.pageNumber);
                 this.setPokemonData([])
-                this.pageNumber = 0
+                this.pageNumber = 1
                 this.fetchPokemonData("/pokemon/?limit=12&offset=0")
                 return 0
             } else {
@@ -83,7 +167,8 @@ class PokemonStore {
         } else {
             if (this.filterInput == "") {
                 console.log("True");
-                this.fetchPokemonData(this.nextUrl)
+                console.log(this.pageNumber);
+                this.fetchPokemonData(`/pokemon/?limit=${this.pageNumber * 12}&offset=0`)
             } else {
                 this.filtered()
             }
@@ -92,4 +177,8 @@ class PokemonStore {
 }
 
 const pokemonStore = new PokemonStore();
+const domStore = new DomStore();
+const detailsStore = new DetailsStore();
+export { domStore, detailsStore };
 export default pokemonStore;
+
